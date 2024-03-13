10963f792f0b80539fd7f31ea8abd346afba815f
kdtTetz <xenosign@naver.com>
2024-03-14 01:40:10 +0900

    240314 템플릿 적용

---
+++ src/main/java/hellp/hellospring/controller/HelloController.java
@@ -0,0 +1,21 @@
+package hellp.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "hello!!");
+        return "hello";
+    }
+
+    @GetMapping("test")
+    public String test(Model model) {
+        model.addAttribute("data", "test!!");
+        return "test";
+    }
+}
