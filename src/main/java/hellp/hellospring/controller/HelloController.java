0ac8f8ee5d27397c2533a0c649ac3a3c76f972d4
kdtTetz <xenosign@naver.com>
2024-03-15 03:06:21 +0900

    240315_빌드_MVC기초

--- src/main/java/hellp/hellospring/controller/HelloController.java
+++ src/main/java/hellp/hellospring/controller/HelloController.java
@@ -3,6 +3,7 @@ package hellp.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -17,5 +18,11 @@ public class HelloController {
     public String test(Model model) {
         model.addAttribute("data", "test!!");
         return "test";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }
